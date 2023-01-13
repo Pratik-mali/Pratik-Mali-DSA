c1e07b00d77ef498b4372b6805dff54b725437f9
Pratik Mali <75832144+Pratik-mali@users.noreply.github.com>
2023-01-13 13:46:27 +0530

    Add files via upload

---
+++ comments.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //This is single line comment
+
+    /* This
+    is Multi-line Comment*/
+    //Comments are user for more information about code and compiler neglect this code
+
+    return 0;
+}
